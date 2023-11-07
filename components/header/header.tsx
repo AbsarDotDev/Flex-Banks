1337402b70fa1fff52f777ee0174eba102e43be7
AhmedDotDev <s.ahmed.ali010@gmail.com>
2023-11-08 02:37:08 +0500

    newsletter bug fixed

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
             </div>
             <div className="w-auto">
               <Link href={'/'}>
-                <video autoPlay loop muted className="w-20 object-cover">
+                <video autoPlay playsInline loop muted className="w-20 object-cover">
                   <source src="/1080p.webm" type="video/mp4" />
                 </video>
               </Link>
